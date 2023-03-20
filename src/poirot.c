f59cbd7c95c80ba77a455581a46601a9d63d6f1e
Felix Horger <felix.horger@kcl.ac.uk>
2023-03-20 20:12:02 +0000

    Even Poirot himself wouldn't be able to figure out which changes I did here

--- src/poirot.c
+++ src/poirot.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include "../include/glad.h"
@@ -33,13 +32,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	window_offset_x = (width - window_length) / 2;
 	window_offset_y = (height - window_length) / 2;
 	glViewport(window_offset_x, window_offset_y, window_length, window_length);
-	return;
-}
-
-char mouse_button_left = 0;
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
-	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) mouse_button_left = 1;
-	else mouse_button_left = 0;
 	return;
 }
 
@@ -92,6 +84,30 @@ void create_program(const char *shader_code[2], GLuint *p_program, GLuint shader
 // TODO: delete shader properly at end
 
 
+char active_plane(double x, double y) {
+	int plane = -1;
+	if (x >= -0.98 && x <= -0.02) {
+		if (y >= 0.02 && y <= 0.98)			plane = 0; // (0, 1, 2)
+		else if (y <= -0.02 && y >= -0.98)	plane = 1; // (0, 2, 1)
+	}
+	else if (x >= 0.02 && x <= 0.98 && y >= 0.02 && y <= 0.98) plane = 2; // (1, 2, 0)
+	return plane;
+}
+
+void plane_0_tex_coords(float tex_coord[2], float window_coord[2], float tex_lower[2], float tex_upper[2]) {
+	tex_coord[0] = (window_coord[0] - 0.02 + 1.0) / 0.96 * (tex_upper[1] - tex_lower[1]) + tex_lower[1];
+	tex_coord[1] = (window_coord[1] - 0.02      ) / 0.96 * (tex_upper[0] - tex_lower[0]) + tex_lower[0];
+	return;
+}
+
+void plane_0_window_coords(float window_coord[2], float tex_coord[2], float tex_lower[2], float tex_upper[2]) {
+	window_coord[0] = (window_coord[0] - tex_lower[1]) * 0.96 / (tex_upper[1] - tex_lower[1]) + 0.02 - 1.0;
+	window_coord[1] = (window_coord[1] - tex_lower[0]) * 0.96 / (tex_upper[0] - tex_lower[0]) + 0.02;
+	return;
+}
+
+
+
 int main(int argc, char* argv[]) {
 
 	// Open window
@@ -112,8 +128,8 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSetMouseButtonCallback(window, mouse_button_callback);
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 	gladLoadGL();
 	glViewport(0, 0, window_length, window_length);
 	glEnable(GL_DEBUG_OUTPUT);
@@ -124,14 +140,14 @@ int main(int argc, char* argv[]) {
 	{
 		const char *shader_code[] = {
 			"\
-				#version 450 core										\n\
-				layout (location = 0) in vec2 in_position;				\n\
-				layout (location = 1) in vec3 in_tex_coordinate;		\n\
-				out vec3 tex_coordinate;								\n\
-				void main() {											\n\
-					gl_Position = vec4(in_position, 0.0, 1.0);			\n\
-					tex_coordinate = in_tex_coordinate;	\n\
-				}														\n\
+				#version 450 core											\n\
+				layout (location = 0) in vec2 position;						\n\
+				layout (location = 1) in vec3 in_tex_coordinate;			\n\
+				out vec3 tex_coordinate;									\n\
+				void main() {												\n\
+					gl_Position = vec4(position, 0.0, 1.0);					\n\
+					tex_coordinate = in_tex_coordinate;						\n\
+				}															\n\
 			",
 			"\
 				#version 450 core										\n\
@@ -190,27 +206,40 @@ int main(int argc, char* argv[]) {
 			"\
 				#version 450 core								\n\
 				layout (location = 0) in vec2 in_position;			\n\
-				layout (location = 1) in vec2 centre;			\n\
-				layout (location = 2) in vec4 in_limits;			\n\
+				layout (location = 1) in vec4 centre;			\n\
+				uniform bool vertical;			\n\
 				out vec2 position;					\n\
-				out vec4 limits;					\n\
+				out vec2 offset;					\n\
 				void main() {									\n\
-					gl_Position = vec4(in_position + centre, 0.0, 1.0);		\n\
+					if (vertical) {            \n\
+						if (abs(centre[0] - centre[2]) > 0.48) { \n\
+							gl_Position = vec4(2.0, 0.0, 0.0, 1.0);		\n\
+							return; \n\
+						} \n\
+						offset[0] = centre[0];        \n\
+						offset[1] = centre[3];        \n\
+					}                             \n\
+					else {                         \n\
+						if (abs(centre[1] - centre[3]) > 0.48) { \n\
+							gl_Position = vec4(2.0, 0.0, 0.0, 1.0);		\n\
+							return; \n\
+						} \n\
+						offset[0] = centre[2]; \n\
+						offset[1] = centre[1];        \n\
+					}                               \n\
+					gl_Position = vec4(in_position + offset, 0.0, 1.0);		\n\
 					position = gl_Position.xy; \n\
-					limits = in_limits;							\n\
 				}												\n\
 			",
 			"\
 				#version 450 core							\n\
 				in vec2 position;					\n\
-				in vec4 limits;					\n\
+				in vec2 offset;					\n\
 				out vec4 colour; \n\
 				void main(void) {							\n\
 					if (									\n\
-						position[0] >= limits[0] &&   \n\
-						position[0] <= limits[1] &&   \n\
-						position[1] >= limits[2] &&   \n\
-						position[1] <= limits[3]   \n\
+						abs(position[0] - offset[0]) <= 0.48 &&   \n\
+						abs(position[1] - offset[1]) <= 0.48       \n\
 					)		colour = vec4(0.0, 1.0, 0.0, 1.0);		\n\
 					else	discard;		\n\
 				}											\n\
@@ -219,23 +248,18 @@ int main(int argc, char* argv[]) {
 		create_program(shader_code, &crosses_program, crosses_shaders);
 	}
 	const float crosses[] = {
-		-0.96f,  0.00f,
-		 0.96f,  0.00f,
-		 0.00f, -0.96f,
-		 0.00f,  0.96f
+		-0.48f,  0.00f,
+		 0.48f,  0.00f,
+		 0.00f, -0.48f,
+		 0.00f,  0.48f
 	};
-	float centres[3][2] = {
-		-0.5f,  0.5f,
-		-0.5f, -0.5f,
-		 0.5f,  0.5f
-	};
-	const float limits[] = {
-		-0.98f, -0.02f,  0.02f,  0.98f, // (0, 1, 2)
-		-0.98f, -0.02f, -0.98f, -0.02f, // (0, 2, 1)
-		 0.02f,  0.98f,  0.02f,  0.98f  // (1, 2, 0)
+	float centres[3][4] = {
+		{-0.5f,  0.5f, -0.5f,  0.5f}, // (0, 1, 2)
+		{-0.5f, -0.5f, -0.5f, -0.5f}, // (0, 2, 1)
+		{ 0.5f,  0.5f,  0.5f,  0.5f}  // (1, 2, 0)
 	};
 
-	float image[100*100*100] = {1};
+	float image[100*100*100] = { 0 };
 
 	GLuint buffers[2];
 	glGenBuffers(2, &buffers[0]);
@@ -255,10 +279,9 @@ int main(int argc, char* argv[]) {
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void *)sizeof(views));
 	// Crosses
 	glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
-	glBufferStorage(GL_ARRAY_BUFFER, sizeof(crosses)+sizeof(centres)+sizeof(limits), NULL, GL_DYNAMIC_STORAGE_BIT);
+	glBufferStorage(GL_ARRAY_BUFFER, sizeof(crosses)+sizeof(centres), NULL, GL_DYNAMIC_STORAGE_BIT);
 	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(crosses), crosses);
 	glBufferSubData(GL_ARRAY_BUFFER, sizeof(crosses), sizeof(centres), centres);
-	glBufferSubData(GL_ARRAY_BUFFER, sizeof(crosses)+sizeof(centres), sizeof(limits), limits);
 	// 
 	GLuint crosses_vertex_array;
 	glGenVertexArrays(1, &crosses_vertex_array);
@@ -267,10 +290,10 @@ int main(int argc, char* argv[]) {
 	glEnableVertexAttribArray(1);
 	glEnableVertexAttribArray(2);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void *)(sizeof(crosses)));
-	glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, 0, (void *)(sizeof(crosses)+sizeof(centres)));
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void *)(sizeof(crosses)));
 	glVertexAttribDivisor(1, 1);
-	glVertexAttribDivisor(2, 1);
+	// Uniform to indicate if vertical branches are drawn
+	GLint cross_vertical = glGetUniformLocation(crosses_program, "vertical");
 	// Texture
 	GLuint texture;
 	glActiveTexture(GL_TEXTURE0);
@@ -278,7 +301,12 @@ int main(int argc, char* argv[]) {
 	glBindTexture(GL_TEXTURE_3D, texture);
 	glTexStorage3D(GL_TEXTURE_3D, 1, GL_R32F, 100, 100, 100);
 	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
+	{
+		long rst_coordinates[3] = {GL_TEXTURE_WRAP_R, GL_TEXTURE_WRAP_S, GL_TEXTURE_WRAP_T};
+		for (int i = 0; i < 3; i++) glTexParameteri(GL_TEXTURE_3D, rst_coordinates[i], GL_CLAMP_TO_BORDER);
+		const float zeros[4] = {0.0, 0.0, 0.0, 0.0};
+		glTexParameterfv(GL_TEXTURE_3D, GL_TEXTURE_BORDER_COLOR, zeros);
+	}
 
 	glClearColor(0, 0, 0, 1);
 	glDisable(GL_DEPTH_TEST);
@@ -291,73 +319,121 @@ int main(int argc, char* argv[]) {
 		image[i] = ((float)rand()) / RAND_MAX;
 		//printf("%f\n", image[i]);
 	}
+	//image[50][99][0] = 1;
 	glTexSubImage3D(GL_TEXTURE_3D, 0, 0, 0, 0, 100, 100, 100, GL_RED, GL_FLOAT, image);
 
 	// Main loop
+	float zoom_incr = 0.005;
+	float shift[3];
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		// Input
-		if (mouse_button_left) {
-			double x, y;
-			glfwGetCursorPos(window, &x, &y);
-			x = 2.0 * (x - window_offset_x) / window_length  - 1.0;
-			y = 1.0 - 2.0 * (y - window_offset_y) / window_length;
-			char update = 1;
-			//printf("%f, %f\n", x, y);
-			if (x >= -0.98 && x <= -0.02) {
-				if (y >= 0.02 && y <= 0.98) { // (0, 1, 2)
-					centres[0][0] = x;
-					centres[0][1] = y;
-					centres[1][0] = x;
-					centres[2][1] = y;
-					for (int i = 0; i < 4; i++) {
-						tex_coords[1][i][1] = (y - 0.02) / 0.96;
-						tex_coords[2][i][0] = (x - 0.02 + 1.0) / 0.96;
-					}
-				}
-				else if (y <= -0.02 && y >= -0.98) { // (0, 2, 1)
-					centres[1][0] = x;
-					centres[1][1] = y;
-					centres[0][0] = x;
-					centres[2][0] = y + 1.0;
-					for (int i = 0; i < 4; i++) {
-						tex_coords[0][i][2] = (y - 0.02 + 1.0) / 0.96;
-						tex_coords[2][i][0] = (x - 0.02 + 1.0) / 0.96;
-					}
-				}
-				else update = 0;
+		// Events
+		char mouse_left_button = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
+		char zoom_in_key = glfwGetKey(window, GLFW_KEY_EQUAL) == GLFW_PRESS;
+		char zoom_out_key = glfwGetKey(window, GLFW_KEY_MINUS) == GLFW_PRESS;
+		char move_up_key = glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS;
+		char move_down_key = glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS;
+		char move_right_key = glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS;
+		char move_left_key = glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS;
+		// React to events
+		if (mouse_left_button || zoom_in_key || zoom_out_key || move_up_key) {
+			// Mouse position and texture coordinates
+			float mouse_window[2];
+			{
+				double x, y;
+				glfwGetCursorPos(window, &x, &y);
+				// Convert to GL coordinates
+				mouse_window[0] = 2.0 * (x - window_offset_x) / window_length - 1.0;
+				mouse_window[1] = 1.0 - 2.0 * (y - window_offset_y) / window_length;
 			}
-			else if (x >= 0.02 && x <= 0.98 && y >= 0.02 && y <= 0.98) { // (0, 2, 1)
-				centres[2][0] = x;
-				centres[2][1] = y;
-				centres[0][1] = y;
-				centres[1][1] = x - 1.0;
-				for (int i = 0; i < 4; i++) {
-					tex_coords[0][i][2] = (x - 0.02) / 0.96;
-					tex_coords[1][i][1] = (y - 0.02) / 0.96;
+			//printf("%f, %f\n", x, y);
+			char plane = active_plane(mouse_window[0], mouse_window[1]);
+			// Execute, dependent on which plane is active
+			char update = 1;
+			float mouse_tex_coord[2];
+			if (plane == 0) { // (0, 1, 2)
+				// Scale according to current zoomed view
+				float mouse_tex_coord[2];
+				plane_0_tex_coords(mouse_tex_coord, mouse_window, tex_coords[0][0], tex_coords[0][2]);
+				if (mouse_left_button) {
+					centres[0][0] = mouse_window[0];
+					centres[0][1] = mouse_window[1];
+					centres[1][0] = mouse_window[0];
+					centres[2][1] = mouse_window[1];
+					for (int i = 0; i < 4; i++) {
+						tex_coords[2][i][0] = mouse_tex_coord[0];
+						tex_coords[1][i][1] = mouse_tex_coord[1];
+					}
+				}
+				if (zoom_in_key || zoom_out_key) {
+					// Change zoom
+					float zoom = 1;
+					if (zoom_in_key)		zoom -= zoom_incr;
+					else if (zoom_out_key)	zoom += zoom_incr;
+					// Change position of crosses
+					// TODO: this formula belongs into a function since used above
+					plane_0_tex_coords(centres[0], centres[0], tex_coords[0][0], tex_coords[0][2]);
+					// Zoom into texture
+					for (int i = 0; i < 4; i++) {
+						for (int j = 0; j < 2; j++) {
+							tex_coords[0][i][1-j] = zoom * (tex_coords[0][i][1-j] - mouse_tex_coord[j]) + mouse_tex_coord[j];
+						}
+					}
+					// TODO: clamp values
+					plane_0_window_coords(centres[0], centres[0], tex_coords[0][0], tex_coords[0][2]);
+					// TODO: store cross centres (in texture coords) as separate variable to avoid numerical error if heavy zooming
+				}
+			}
+			else if (plane == 1) { // (0, 2, 1)
+				if (mouse_left_button) {
+					centres[1][0] = mouse_window[0];
+					centres[1][1] = mouse_window[1];
+					centres[0][0] = mouse_window[0];
+					centres[2][0] = mouse_window[1] + 1.0;
+					for (int i = 0; i < 4; i++) {
+						tex_coords[0][i][2] = (mouse_window[1] - 0.02 + 1.0) / 0.96;
+						tex_coords[2][i][0] = (mouse_window[0] - 0.02 + 1.0) / 0.96;
+					}
+				}
+			}
+			else if (plane == 2) { // (1, 2, 0)
+				if (mouse_left_button) {
+					centres[2][0] = mouse_window[0];
+					centres[2][1] = mouse_window[1];
+					centres[0][1] = mouse_window[1];
+					centres[1][1] = mouse_window[0] - 1.0;
+					for (int i = 0; i < 4; i++) {
+						tex_coords[0][i][2] = (mouse_window[0] - 0.02) / 0.96;
+						tex_coords[1][i][1] = (mouse_window[1] - 0.02) / 0.96;
+					}
 				}
 			}
 			else update = 0;
 			if (update) {
-				// TODO don't copy all
+				// TODO don't copy all?
 				glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
 				glBufferSubData(GL_ARRAY_BUFFER, sizeof(views), sizeof(tex_coords), tex_coords);
 				glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
 				glBufferSubData(GL_ARRAY_BUFFER, sizeof(crosses), sizeof(centres), centres);
 			}
 		}
+		// Esc
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, 1);
 		// Views
 		glUseProgram(views_program);
 		glBindVertexArray(views_vertex_array);
 		for (int i = 0; i <= 8; i += 4) glDrawArrays(GL_TRIANGLE_FAN, i, 4);
 		glUseProgram(crosses_program);
 		glBindVertexArray(crosses_vertex_array);
+		glUniform1i(cross_vertical, 0); 
 		glDrawArraysInstanced(GL_LINES, 0, 2, 3);
+		glUniform1i(cross_vertical, 1); 
 		glDrawArraysInstanced(GL_LINES, 2, 2, 3);
 		glFlush();
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, 1);
+		//glfwWaitEvents();
 	}
 
 	// Clean up
